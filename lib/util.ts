3f76bbf18874dd5c2cc6ff27d14d5ddbab17d5ad
chenmh <chenmh@sucsoft.com>
2022-08-11 17:35:35 +0800

    fix: string to number

--- lib/util.ts
+++ lib/util.ts
@@ -1,3 +1,5 @@
+import { ClockConfig } from "./useClock";
+
 export const minmax = (value: number, min: number, max: number, fixed: number = 0) => {
     return parseFloat(Math.min(max, Math.max(min, value)).toFixed(fixed))
 }
@@ -8,4 +10,17 @@ export const shallowMerge = <T extends { [k: string]: any }>(optionalValue: Part
         copy[k] = optionalValue[k]!
     }
     return copy
+}
+
+const KeysOfValueInNumber: string[] = [
+    'dialRadius', 'dialStrokeWidth',
+    'hourPercent', 'hourTail', 'hourStrokeWidth',
+    'minutePercent', 'minuteTail', 'minuteStrokeWidth',
+    'secondPercent', 'secondTail', 'secondStrokeWidth'
+]
+export const fixStringToNumber = (ori: { [k: string]: any }): Partial<ClockConfig> => {
+    KeysOfValueInNumber.forEach(key => {
+        if(ori[key] !== undefined) ori[key] = parseFloat(ori[key] + '')
+    })
+    return ori as ClockConfig
 }
